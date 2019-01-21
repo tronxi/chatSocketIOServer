d7ebb38d906b73a110e59b25c4d90a89253425f4
tronxi <sergio.gsanchez97@gmail.com>
2019-01-21 13:57:32 +0100

    multiples salas

--- app.js
+++ app.js
@@ -14,8 +14,20 @@ app.get('/', (req, res) =>
 });
 io.on('connect', function(socket) {
     console.log('Un cliente se ha conectado');
-    socket.on('new-message', function(socket) {
-        console.log(socket);
-        io.sockets.emit('message', socket);
+    socket.on('new-message', function(data) {
+        let json = JSON.parse(data);
+        console.log(json.sala);
+        console.log(json.mensaje);
+        io.sockets.in(json.sala).emit('message', json.mensaje);
+    });
+    socket.on('union', function(data){
+        socket.join(data);
+        console.log('unido a sala ' + data);
+        //io.sockets.in(data).emit('message', 'socket');
+    });
+    socket.on('salir', function(data){
+        socket.leave(data);
+        console.log('salir a sala ' + data);
+        //io.sockets.in(data).emit('message', 'socket');
     });
 });
